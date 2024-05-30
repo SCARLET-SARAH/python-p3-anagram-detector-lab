011a2ad59d8c52f2cd1dae68c9612133c10d2bf9
scarlet <sarahscarlet641@gmail.com>
2024-05-30 11:01:42 +0300

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
 # your code goes here!
+def __init__(self, word):
+        self.word = word
+
+def match(self, words):
+        anagrams = [word for word in words if sorted(word) == sorted(self.word)]
+        return anagrams
